ae5202d16c399b8bb36aecde2fb2b96a6c76741e
dummycount <dummymal@hotmail.com>
2026-05-08 23:10:34 -0500

    Switch to stable version of System.CommandLine

--- csharp/PythonNetStubTool/Program.cs
+++ csharp/PythonNetStubTool/Program.cs
@@ -1,55 +1,96 @@
-﻿using PythonNetStubGenerator;
+﻿using System.CommandLine;
 
-namespace PythonNetStubTool
+namespace PythonNetStubGenerator.Tool;
+
+static class Program
 {
-    static class Program
+    static int Main(string[] args)
     {
-        /// <summary>
-        /// Creates stubs for Python.Net
-        /// </summary>
-        /// <param name="destPath">Path to save the subs to.</param>
-        /// <param name="searchPaths">Path to search for referenced assemblies</param>
-        /// <param name="targetDlls">Target DLLsz</param>
-        /// <param name="forceLf">Force LF line endings for stubs</param>
-        static int Main(
-            DirectoryInfo destPath,
-            string targetDlls,
-            DirectoryInfo[]? searchPaths = null,
-            bool forceLf = false
-            )
+		Argument<FileInfo[]> targetDlls = new("dll")
+		{
+			Description = "Target DLLs",
+			Arity = ArgumentArity.OneOrMore,
+		};
+
+		Option<DirectoryInfo> outPathArg = new("--out", "-o")
         {
-            if (searchPaths != null)
-            {
-                foreach (var searchPath in searchPaths)
-                    Console.WriteLine($"search path {searchPath}");
-            }
+            Description = "Path to save the stubs to [default: ./stubs]",
+        };
 
-            var infos = new List<FileInfo>();
-            foreach (var pathStr in targetDlls.Split(','))
-            {
-                var assemblyPath = new FileInfo(pathStr);
-                if (!assemblyPath.Exists)
-                {
-                    Console.WriteLine($"error: can not find {assemblyPath}");
-                    return -1;
-                }
-                infos.Add(assemblyPath);
+        Option<DirectoryInfo[]> searchPaths = new("--search", "-s")
+        {
+            Description = "Paths to search for referenced assemblies",
+            Arity = ArgumentArity.ZeroOrMore,
+        };
 
-            }
-            
-            Console.WriteLine($"building stubs...");
+        Option<bool> forceLfOption = new("--force-lf")
+        {
+            Description = "Force LF line endings for stubs [default: false]"
+        };
 
-            try
+        RootCommand rootCommand = new("Creates stubs for Python.Net")
+        {
+            targetDlls,
+            outPathArg,
+            searchPaths,
+        };
+
+        rootCommand.SetAction((result) => WriteStubs(
+            destPath: result.GetValue(outPathArg), 
+            targetDlls: result.GetValue(targetDlls), 
+            searchPaths: result.GetValue(searchPaths), 
+            forceLf: result.GetValue(forceLfOption)
+        ));
+
+        var result = rootCommand.Parse(args);
+        return result.Invoke();
+    }
+
+
+    static int WriteStubs(
+        DirectoryInfo? destPath,
+        FileInfo[]? targetDlls,
+        DirectoryInfo[]? searchPaths = null,
+        bool forceLf = false
+        )
+    {
+        destPath ??= new DirectoryInfo("stubs");
+
+        if (targetDlls is null)
+        {
+            Console.WriteLine("error: requires at least one DLL path");
+            return -1;
+        }
+
+        if (searchPaths is not null)
+        {
+            foreach (var searchPath in searchPaths)
             {
-                var dest = StubBuilder.BuildAssemblyStubs(destPath, infos.ToArray(), searchPaths, forceLf);
-                Console.WriteLine($"stubs saved to {dest}");
-                return 0;
+                Console.WriteLine($"search path {searchPath}");
             }
-            catch (Exception sgEx)
+        }
+
+        foreach (var path in targetDlls)
+        {
+            if (!path.Exists)
             {
-                Console.WriteLine($"error: failed generating stubs | {sgEx.Message}");
-                throw;
+                Console.WriteLine($"error: can not find {path}");
+                return -1;
             }
+        }
+
+        Console.WriteLine($"building stubs...");
+
+        try
+        {
+            var dest = StubBuilder.BuildAssemblyStubs(destPath, targetDlls, searchPaths, forceLf);
+            Console.WriteLine($"stubs saved to {dest}");
+            return 0;
+        }
+        catch (Exception sgEx)
+        {
+            Console.WriteLine($"error: failed generating stubs | {sgEx.Message}");
+            throw;
         }
     }
 }
